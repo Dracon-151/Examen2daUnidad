8341f2c0e2c5b194eba7847e75a4b4fc51b84e8c
Dracon-151 <demianalexisalvarado@gmail.com>
2022-08-30 22:56:32 -0600

    Sistema base y modificacion de assets
    Contador de tiempo
    Sistema de puntos por tiempo
    Pantalla de victoria
    Ajustes en assets

--- game.php
+++ game.php
@@ -4,7 +4,7 @@
         <title>Canvas</title>
     </head>
     <body>
-        <canvas width="800" height="600" id="cvs">
+        <canvas width="1335" height="600" id="cvs">
           Hola tu navegador no funciona
         </canvas>
 
@@ -16,13 +16,20 @@
         var paredes = null;
         var direccion = 1;
         var puntos = 0;
-        var speed = 5;
+        var puntuacion = 0;
+        var tiempo = 0;
+        var speed = 4;
         var pause = false;
+        var win = false;
 
         var bot = new Image();
         var wallx = new Image();
         var wally = new Image();
         var gear = new Image();
+
+        var tiempoInicio;
+        var tiempoTotal = 0;
+        var tiempoPausa = 0;
 
         var sound = new Audio();
         
@@ -31,14 +38,14 @@
             canvas = document.getElementById('cvs');
             ctx = canvas.getContext('2d');
             
-            bot.src = 'img/PlayerSprite.png';
+            bot.src = 'img/Player.png';
             wallx.src = 'img/platformX.png';
             wally.src = 'img/platformY.png';
             gear.src = 'img/gear.png';
 
-            sound.src = 'audio/gearSound.mp3';
+            sound.src = 'audio/recolectar.wav';
 
-            player1 = new Cuadro(200,200,38,40,bot);
+            player1 = new Cuadro(50,100,38,40,bot);
             player2 = new Cuadro(300,300,28,28,gear);
             paredes = [new Cuadro(80,250,25,101,wally), 
                     new Cuadro(720,250,25,101,wally),  
@@ -60,7 +67,6 @@
 
         document.addEventListener("keydown", function(e){
             console.log(e.keyCode);
-
             if(!pause){
                 if(e.keyCode == 37 || e.keyCode == 65){
                     direccion = 2;
@@ -75,26 +81,51 @@
                     direccion = 3;
                 }
             }
-            if(e.keyCode == 32){
+            if(e.keyCode == 32 && !win){
                 pause = !pause;
+            }
+            if(e.keyCode == 82){
+                location.reload();
             }
         });
 
 
 
-        function paint(){
+        function paint(currentTime){
+
+            if(!tiempoInicio) tiempoInicio = currentTime;
+
+            if(!pause){
+                tiempoTotal = (currentTime - tiempoInicio) - tiempoPausa;
+            }else if(pause){
+                tiempoPausa = (currentTime - tiempoInicio) - tiempoTotal;
+                tiempoTotal = (currentTime - tiempoInicio) - tiempoPausa;
+            }
+
+            
+            tiempo = (tiempoTotal/1000).toFixed(1);
+            puntuacion = (puntos-(tiempo*5)).toFixed(0);
+            if(puntuacion < 0) puntuacion = 0;
+
+
+            /*if(tiempo > 2) {
+                pause = true;
+                win = true;
+            }*/
 
             window.requestAnimationFrame(paint);
-            ctx.fillStyle = "rgb(255,255,255)";
-            ctx.fillRect(0,0,800,600);
+            ctx.fillStyle = "rgb(153, 113, 166)";
+            ctx.fillRect(0,0,1335,600);
             
-            ctx.font = "20px Arial";
-            ctx.fillStyle = "rgb(100,100,100)";
-            ctx.fillText("Puntos: " + puntos, 50, 50);
-
+           
             bounds();
 
             if(!pause){
+                ctx.font = "20px Arial";
+                ctx.fillStyle = "rgb(255,255,255)";
+                ctx.fillText("Tiempo: " + tiempo + 's', 25, 25);
+                if(puntuacion < 60) ctx.fillStyle = "rgb(255,138,138)";
+                ctx.fillText("Puntuación: " + puntuacion + " puntos", 25, 50);
                 move();
             }
 
@@ -126,30 +157,42 @@
                 sound.play();
                 player2.x = Math.random()* 500 + 100;
                 player2.y = Math.random()* 200 + 200;
-                puntos += 10;
-                speed += 0.5;
+                puntos += 50;
             }
             player2.paint(ctx);
 
-            if(pause){
-                ctx.fillStyle = "rgba(0,0,0,0.4)";
-                ctx.fillRect(0,0,800,600);
+            if(pause && !win){
+                ctx.fillStyle = "rgba(0,0,0,0.6)";
+                ctx.fillRect(0,0,1350,600);
                 
                 ctx.font = "20px Arial";
                 ctx.fillStyle = "rgb(255,255,255)";
-                ctx.fillText("Pausa", 350, 320);
+                ctx.fillText("Pausa", 675, 320);
+            }
+            if(pause && win){
+                ctx.fillStyle = "rgba(138,138,209,0.4)";
+                ctx.fillRect(0,0,1350,600);
+                
+                ctx.font = "50px Arial";
+                ctx.fillStyle = "rgb(255,255,255)";
+                ctx.fillText("¡Has ganado!", 505, 290);
+                ctx.font = "30px Arial";
+                ctx.fillText("Tiempo: " + tiempo + 's', 570 , 335);
+                ctx.fillText("Puntuación: " + puntuacion + " puntos", 520, 370);
+                ctx.font = "15px Arial";
+                ctx.fillText("Presiona R para reiniciar", 570, 415);
             }
         }
 
         function bounds(){
-            if(player1.x > 800) player1.x = 0;
-            if(player1.y > 600) player1.y = 0;
-            if(player1.x < 0) player1.x = 800;
-            if(player1.y < 0) player1.y = 600;
-            if(player2.x > 800) player2.x = 0;
-            if(player2.y > 600) player2.y = 0;
-            if(player2.x < 0) player2.x = 800;
-            if(player2.y < 0) player2.y = 600;
+            if(player1.x > 1290) player1.x = 1290;
+            if(player1.y > 550) player1.y = 550;
+            if(player1.x < 10) player1.x = 10;
+            if(player1.y < 60) player1.y = 60;
+            if(player2.x > 1290) player2.x = 1290;
+            if(player2.y > 550) player2.y = 550;
+            if(player2.x < 10) player2.x = 10;
+            if(player2.y < 60) player2.y = 60;
         }
 
         function move(){
