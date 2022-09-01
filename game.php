b3dc0b6763e1965bb524b16d9500bfdfd8f74ff3
Dracon-151 <demianalexisalvarado@gmail.com>
2022-08-31 19:38:57 -0600

    Musica, sonido victoria y paredes laberinto y arreglo de monedas

--- game.php
+++ game.php
@@ -12,13 +12,14 @@
         var canvas = null;
         var ctx = null;
         var player1 = null;
-        var player2 = null;
+        var gears = null;
         var paredes = null;
+        var station = null;
         var direccion = 1;
         var puntos = 0;
         var puntuacion = 0;
         var tiempo = 0;
-        var speed = 4;
+        var speed = 0;
         var pause = false;
         var win = false;
 
@@ -26,12 +27,15 @@
         var wallx = new Image();
         var wally = new Image();
         var gear = new Image();
+        var charge = new Image();
 
         var tiempoInicio;
         var tiempoTotal = 0;
         var tiempoPausa = 0;
 
-        var sound = new Audio();
+        var gearSound = new Audio();
+        var music = new Audio();
+        var victory = new Audio();
         
         function run()
         {
@@ -42,15 +46,57 @@
             wallx.src = 'img/platformX.png';
             wally.src = 'img/platformY.png';
             gear.src = 'img/gear.png';
+            charge.src = 'img/chargestation.png';
 
-            sound.src = 'audio/recolectar.wav';
+            gearSound.src = 'audio/recolectar.wav';
+            music.src = 'audio/bgMusic.wav';
+            gearSound.src = 'audio/recolectar.wav';
+            victory.src = 'audio/victory.mp3';
 
-            player1 = new Cuadro(50,100,38,40,bot);
-            player2 = new Cuadro(300,300,28,28,gear);
-            paredes = [new Cuadro(80,250,25,101,wally), 
-                    new Cuadro(720,250,25,101,wally),  
-                    new Cuadro(350,80,101,25,wallx), 
-                    new Cuadro(350,500,101,25,wallx)];
+            player1 = new Cuadro(60,75,38,40,bot);
+            gears = [new Cuadro(300,300,28,28,gear), new Cuadro(500,500,28,28,gear)];
+            paredes = [new Cuadro(0,60,25,101,wally), 
+                    new Cuadro(0,161,25,101,wally),
+                    new Cuadro(0,262,25,101,wally),
+                    new Cuadro(0,363,25,101,wally),
+                    new Cuadro(0,464,25,101,wally),
+                    new Cuadro(0,565,25,101,wally),
+
+                    new Cuadro(1310,60,25,101,wally), 
+                    new Cuadro(1310,161,25,101,wally),
+                    new Cuadro(1310,262,25,101,wally),
+                    new Cuadro(1310,363,25,101,wally),
+                    new Cuadro(1310,464,25,101,wally),
+                    new Cuadro(1310,565,25,101,wally),
+
+                    new Cuadro(126,60,101,25,wallx),
+                    new Cuadro(227,60,101,25,wallx),
+                    new Cuadro(328,60,101,25,wallx),
+                    new Cuadro(429,60,101,25,wallx),
+                    new Cuadro(530,60,101,25,wallx),
+                    new Cuadro(631,60,101,25,wallx),
+                    new Cuadro(732,60,101,25,wallx),
+                    new Cuadro(833,60,101,25,wallx),
+                    new Cuadro(934,60,101,25,wallx),
+                    new Cuadro(1035,60,101,25,wallx),
+                    new Cuadro(1136,60,101,25,wallx),
+                    new Cuadro(1237,60,101,25,wallx),
+                
+                    
+                    new Cuadro(0,575,101,25,wallx),
+                    new Cuadro(101,575,101,25,wallx),
+                    new Cuadro(202,575,101,25,wallx),
+                    new Cuadro(303,575,101,25,wallx),
+                    new Cuadro(404,575,101,25,wallx),
+                    new Cuadro(505,575,101,25,wallx),
+                    new Cuadro(606,575,101,25,wallx),
+                    new Cuadro(707,575,101,25,wallx),
+                    new Cuadro(808,575,101,25,wallx),
+                    new Cuadro(909,575,101,25,wallx),
+                    new Cuadro(1010,575,101,25,wallx),
+                    new Cuadro(1111,575,101,25,wallx)];
+                    
+            station = new Cuadro(1210,525,101,72,charge);
             paint();
         }
 
@@ -67,6 +113,7 @@
 
         document.addEventListener("keydown", function(e){
             console.log(e.keyCode);
+            if(speed == 0) speed = 4;
             if(!pause){
                 if(e.keyCode == 37 || e.keyCode == 65){
                     direccion = 2;
@@ -93,6 +140,8 @@
 
         function paint(currentTime){
 
+            if(!win)music.play();
+
             if(!tiempoInicio) tiempoInicio = currentTime;
 
             if(!pause){
@@ -108,10 +157,12 @@
             if(puntuacion < 0) puntuacion = 0;
 
 
-            /*if(tiempo > 2) {
+            if(player1.se_tocan(station) && !win){
                 pause = true;
                 win = true;
-            }*/
+                music.pause();
+                victory.play();
+            }
 
             window.requestAnimationFrame(paint);
             ctx.fillStyle = "rgb(153, 113, 166)";
@@ -149,39 +200,49 @@
                 }
             }
 
+            for (var i = 0; i < gears.length; i++) {
+                gears[i].paint(ctx);
+                if(player1.se_tocan(gears[i])){
+                    gearSound.pause();
+                    gearSound.currentTime = 0;
+                    gearSound.play();
+                    puntos += 50;
+                    gears.splice(i, 1);
+                }
+            }
+
+            station.paint(ctx);
             player1.paint(ctx);
 
-            if(player1.se_tocan(player2)){
-                sound.pause();
-                sound.currentTime = 0;
-                sound.play();
-                player2.x = Math.random()* 500 + 100;
-                player2.y = Math.random()* 200 + 200;
-                puntos += 50;
-            }
-            player2.paint(ctx);
-
             if(pause && !win){
-                ctx.fillStyle = "rgba(0,0,0,0.6)";
-                ctx.fillRect(0,0,1350,600);
-                
-                ctx.font = "20px Arial";
-                ctx.fillStyle = "rgb(255,255,255)";
-                ctx.fillText("Pausa", 675, 320);
+                pausa();
             }
             if(pause && win){
-                ctx.fillStyle = "rgba(138,138,209,0.4)";
-                ctx.fillRect(0,0,1350,600);
-                
-                ctx.font = "50px Arial";
-                ctx.fillStyle = "rgb(255,255,255)";
-                ctx.fillText("¡Has ganado!", 505, 290);
-                ctx.font = "30px Arial";
-                ctx.fillText("Tiempo: " + tiempo + 's', 570 , 335);
-                ctx.fillText("Puntuación: " + puntuacion + " puntos", 520, 370);
-                ctx.font = "15px Arial";
-                ctx.fillText("Presiona R para reiniciar", 570, 415);
+                winscreen();
             }
+        }
+
+        function pausa(){
+            ctx.fillStyle = "rgba(0,0,0,0.6)";
+            ctx.fillRect(0,0,1350,600);
+            
+            ctx.font = "20px Arial";
+            ctx.fillStyle = "rgb(255,255,255)";
+            ctx.fillText("Pausa", 675, 320);
+        }
+
+        function winscreen(){
+            ctx.fillStyle = "rgba(138,138,209,0.4)";
+            ctx.fillRect(0,0,1350,600);
+            
+            ctx.font = "50px Arial";
+            ctx.fillStyle = "rgb(255,255,255)";
+            ctx.fillText("¡Has ganado!", 505, 290);
+            ctx.font = "30px Arial";
+            ctx.fillText("Tiempo: " + tiempo + 's', 570 , 335);
+            ctx.fillText("Puntuación: " + puntuacion + " puntos", 520, 370);
+            ctx.font = "15px Arial";
+            ctx.fillText("Presiona R para reiniciar", 570, 415);
         }
 
         function bounds(){
@@ -189,10 +250,6 @@
             if(player1.y > 550) player1.y = 550;
             if(player1.x < 10) player1.x = 10;
             if(player1.y < 60) player1.y = 60;
-            if(player2.x > 1290) player2.x = 1290;
-            if(player2.y > 550) player2.y = 550;
-            if(player2.x < 10) player2.x = 10;
-            if(player2.y < 60) player2.y = 60;
         }
 
         function move(){
